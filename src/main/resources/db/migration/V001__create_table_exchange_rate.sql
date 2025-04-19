95190ad3742b2e98f9d65fd42912989afdecb677
Ewerton Dias <ewertondsdias@gmail.com>
2025-04-19 00:44:21 -0300

    feat: Add flyway and create database

---
+++ src/main/resources/db/migration/V001__create_table_exchange_rate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS exchange_rate (
+    id                  UUID            PRIMARY KEY NOT NULL,
+    rate_base           VARCHAR(3)      NOT NULL,
+    rate_target         VARCHAR(3)      NOT NULL,
+    rate_conversion     NUMERIC(10, 2)  NOT NULL,
+    date_consultation   TIMESTAMP       NOT NULL
+);
